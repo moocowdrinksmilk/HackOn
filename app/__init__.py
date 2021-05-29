b973eb4e4691b9085e6e7295fb7b662029bab15e
bingyuyap <bingyu.yap.21@gmail.com>
2021-05-29 13:40:14 +0800

    Clean code

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 import telegram
 
-# from app.config import TOKEN, URL
+from app.config import TOKEN, URL
 
 global bot
 
@@ -13,27 +13,19 @@ from flask import Flask,request
 import requests
 from app import app, bot
 
-
-
-app = Flask(__name__)
-
 @app.route("/",methods=["POST","GET"])
 def index():
     if(request.method == "POST"):
        
         resp = request.get_json()
-        # msgtext = resp["message"]["text"]
-        # sendername = resp["message"]["from"]["first_name"]
+        msgtext = resp["message"]["text"]
+        sendername = resp["message"]["from"]["first_name"]
         chatid = resp["message"]["chat"]["id"]
-        # return resp
         bot.sendMessage(chat_id=chatid, text='Welcum!')
         print
         print(resp)
+
     return "suck my cock"
-
-
-def sendmessage(chatid):
-    print(chatid)
 
 @app.route("/setwebhook/")
 def setwebhook():
@@ -47,21 +39,21 @@ def setwebhook():
 
 
 
-@app.route('/{}'.format(key), methods=['POST'])
-def respond():
-    update = telegram.Update.de_json(request.get_json(force=True), bot)
+# @app.route('/{}'.format(key), methods=['POST'])
+# def respond():
+#     update = telegram.Update.de_json(request.get_json(force=True), bot)
 
-    chat_id =  update.message.chat.id
-    msg_id = update.message.message_id
+#     chat_id =  update.message.chat.id
+#     msg_id = update.message.message_id
 
-    text = update.message.text.encode('utf-8').decode()
-    print("got text message: ", text)
+#     text = update.message.text.encode('utf-8').decode()
+#     print("got text message: ", text)
 
-    if text == '/start':
-        bot.sendMessage(chat_id=chat_id, text=messages.bot_welcome, reply_to_message_id=msg_id)
-    else:
-        #bot logic here
-        bot.sendMessage(chat_id=chat_id, text=messages.bot_temp, reply_to_message_id=msg_id)
+#     if text == '/start':
+#         bot.sendMessage(chat_id=chat_id, text=messages.bot_welcome, reply_to_message_id=msg_id)
+#     else:
+#         #bot logic here
+#         bot.sendMessage(chat_id=chat_id, text=messages.bot_temp, reply_to_message_id=msg_id)
 
-    return 'wtv, doesnt matter'
+#     return 'wtv, doesnt matter'
 
